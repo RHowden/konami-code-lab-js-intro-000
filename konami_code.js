8140a1e1bd73361068184242cca4b72b621ae078
Robert Howden <robert.howden@current-rms.com>
2020-04-22 09:15:35 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,25 +10,19 @@ const codes = [
   "b",
   "a"
 ];
-
 let index = 0;
-
 function onKeyDownHandler(e) {
   const key = e.key;
-
   if (key === codes[index]) {
     index++;
-
     if (index === codes.length) {
       alert("Hurray!");
-
       index = 0;
     }
   } else {
     index = 0;
   }
 }
-
 function init() {
   onKeyDownHandler();
 }
